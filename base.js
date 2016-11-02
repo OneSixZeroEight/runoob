b93278749feff0c4ca0552055e718cd735bd7a7a
wsscat <2587575267@qq.com>
2016-11-02 10:44:32 +0800

    add demo

--- base.js
+++ base.js
@@ -1,12 +1,12 @@
 var app = angular.module('wsscat', ['ngRoute']);
-app.config(['$routeProvider',function($routeProvider){
-			$routeProvider.when('/detail1',{
-				templateUrl:'views/detail1.html',
-				//controller:'indexCtrl'
-			}).when('/detail2',{
-				templateUrl:'views/detail2.html',
-				//controller:'homeCtrl'
-			})
+app.config(['$routeProvider', function($routeProvider) {
+	$routeProvider.when('/detail1', {
+		templateUrl: 'views/detail1.html',
+		//controller:'indexCtrl'
+	}).when('/detail2', {
+		templateUrl: 'views/detail2.html',
+		//controller:'homeCtrl'
+	})
 }])
 app.controller('indexCtrl', function($scope, $http) {
 	//	(adsbygoogle = window.adsbygoogle || []).push({});
@@ -17,11 +17,11 @@ app.controller('indexCtrl', function($scope, $http) {
 	$scope.name = 'wsscat'
 	$scope.fuck = '菜鸟教程 -- 学的不仅是技术，更是梦想！'
 	$scope.navs = [{
-		name: '首页',
-		url: 'http://www.runoob.com/'
+		name: '第一个DEMO页面',
+		url: '1.html#/detail1'
 	}, {
-		name: '首页',
-		url: 'http://www.runoob.com/'
+		name: '第二个DEMO页面',
+		url: '2.html#/detail2'
 	}, {
 		name: '首页',
 		url: 'http://www.runoob.com/'
@@ -52,11 +52,11 @@ app.controller('index2Ctrl', function($scope, $http) {
 	$scope.name = 'wsscat'
 	$scope.fuck = '菜鸟教程 -- 学的不仅是技术，更是梦想！'
 	$scope.navs = [{
-		name: '第二个页面',
-		url: 'http://www.runoob.com/'
+		name: '第一个DEMO页面',
+		url: '1.html#/detail1'
 	}, {
-		name: '第二个页面',
-		url: 'http://www.runoob.com/'
+		name: '第二个DEMO页面',
+		url: '2.html#/detail2'
 	}, {
 		name: '第二个页面',
 		url: 'http://www.runoob.com/'
@@ -106,5 +106,18 @@ app.directive('xcontent', function() {
 		link: function(scope, ele, attr) {
 			ele.css('color', 'red')
 		}
+	}
+})
+
+app.directive('xfooter', function() {
+	return {
+		templateUrl: 'directive/xfooter.html',
+		link: function(scope, ele, attr) {}
+	}
+})
+
+app.directive('fixedbtn', function() {
+	return {
+		templateUrl: 'directive/fixedbtn.html'
 	}
 })
