c817cdfa1747c7a506ac4d137e2ccc7c1dadba77
wsscat <2587575267@qq.com>
2016-11-02 10:19:26 +0800

    add runoo demo

---
+++ base.js
@@ -0,0 +1,110 @@
+var app = angular.module('wsscat', ['ngRoute']);
+app.config(['$routeProvider',function($routeProvider){
+			$routeProvider.when('/detail1',{
+				templateUrl:'views/detail1.html',
+				//controller:'indexCtrl'
+			}).when('/detail2',{
+				templateUrl:'views/detail2.html',
+				//controller:'homeCtrl'
+			})
+}])
+app.controller('indexCtrl', function($scope, $http) {
+	//	(adsbygoogle = window.adsbygoogle || []).push({});
+	console.log($scope);
+	$scope.methods = function() {
+
+	}
+	$scope.name = 'wsscat'
+	$scope.fuck = '菜鸟教程 -- 学的不仅是技术，更是梦想！'
+	$scope.navs = [{
+		name: '首页',
+		url: 'http://www.runoob.com/'
+	}, {
+		name: '首页',
+		url: 'http://www.runoob.com/'
+	}, {
+		name: '首页',
+		url: 'http://www.runoob.com/'
+	}, {
+		name: '首页',
+		url: 'http://www.runoob.com/'
+	}, {
+		name: '首页',
+		url: 'http://www.runoob.com/'
+	}, {
+		name: '首页',
+		url: 'http://www.runoob.com/'
+	}, {
+		name: '首页',
+		url: 'http://www.runoob.com/'
+	}, {
+		name: '首页',
+		url: 'http://www.runoob.com/'
+	}, {
+		name: '首页',
+		url: 'http://www.runoob.com/'
+	}, {
+		name: '首页',
+		url: 'http://www.runoob.com/'
+	}]
+})
+app.controller('index2Ctrl', function($scope, $http) {
+	$scope.name = 'wsscat'
+	$scope.fuck = '菜鸟教程 -- 学的不仅是技术，更是梦想！'
+	$scope.navs = [{
+		name: '第二个页面',
+		url: 'http://www.runoob.com/'
+	}, {
+		name: '第二个页面',
+		url: 'http://www.runoob.com/'
+	}, {
+		name: '第二个页面',
+		url: 'http://www.runoob.com/'
+	}, {
+		name: '第二个页面',
+		url: 'http://www.runoob.com/'
+	}, {
+		name: '第二个页面',
+		url: 'http://www.runoob.com/'
+	}, {
+		name: '第二个页面',
+		url: 'http://www.runoob.com/'
+	}, {
+		name: '第二个页面',
+		url: 'http://www.runoob.com/'
+	}, {
+		name: '第二个页面',
+		url: 'http://www.runoob.com/'
+	}, {
+		name: '第二个页面',
+		url: 'http://www.runoob.com/'
+	}, {
+		name: '第二个页面',
+		url: 'http://www.runoob.com/'
+	}]
+})
+app.directive('xheader', function() {
+	return {
+		templateUrl: 'directive/xheader.html',
+		link: function(scope, ele, attr) {
+			console.log(ele)
+		}
+	}
+})
+app.directive('xnav', function() {
+	return {
+		templateUrl: 'directive/xnav.html',
+		link: function(scope, ele, attr) {
+			console.log(ele)
+		}
+	}
+})
+
+app.directive('xcontent', function() {
+	return {
+		templateUrl: 'directive/xcontent.html',
+		link: function(scope, ele, attr) {
+			ele.css('color', 'red')
+		}
+	}
+})
